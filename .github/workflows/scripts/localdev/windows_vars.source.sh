fec3b4eb9c1ba541e27713da0884ef61c2376d22
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2022-04-29 15:52:37 -0700

    Update diskuv-opam-repository
    New package version: Add ocaml-option-32bit.2 that supports Windows

--- .github/workflows/scripts/localdev/windows_vars.source.sh
+++ .github/workflows/scripts/localdev/windows_vars.source.sh
@@ -11,7 +11,7 @@ fi
 export bootstrap_opam_version="0.0.0"
 export opam_abi=windows_x86 #_64
 
-export DEFAULT_DISKUV_OPAM_REPOSITORY_TAG=2ad0ad744d7a85c5a1651d9b0b254590096cf50c # "v0.4.0-prerel19_r5"
+export DEFAULT_DISKUV_OPAM_REPOSITORY_TAG=7a04f7578edaf4c3fb52d4d877ef9596b858242d # "v0.4.0-prerel19_r6"
 export PIN_OCAMLFIND=1.9.1
 export PIN_OCAMLBUILD=0.14.0
 export INSTALLERNAME=ocaml
