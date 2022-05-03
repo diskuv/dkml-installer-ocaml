abd14fefaa0fe68bf521f33d13095db4df11f637
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2022-05-03 00:09:00 -0700

    Don't use installation prefix override

--- installer/src/private_common.ml
+++ installer/src/private_common.ml
@@ -11,4 +11,6 @@ let program_name =
     Dkml_package_console_common.name_full = "Diskuv OCaml";
     name_camel_case_nospaces = "DiskuvOCaml";
     name_kebab_lower_case = "diskuv-ocaml";
+    installation_prefix_camel_case_nospaces_opt = None;
+    installation_prefix_kebab_lower_case_opt = None;
   }
