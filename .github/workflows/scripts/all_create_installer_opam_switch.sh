738def5a1c1a09fdfc860769e681fea4887ec251
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2022-05-09 08:25:48 -0700

    Remove unused extra-maintenance scripts

--- .github/workflows/scripts/all_create_installer_opam_switch.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -euf
-set -x
-[ -d "$OPAMROOT/installer-$INSTALLERNAME/.opam-switch" ] || PATH="$PWD/bootstrap/bin:$PATH" opam switch create installer-"$INSTALLERNAME" --repos diskuv,default --empty
