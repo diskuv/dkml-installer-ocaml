1835637801f9629740bb1c4f0ad1a19480eabb46
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2022-11-23 17:51:36 -0800

    Use Write-Output in Vagrant

--- vagrant/win32/test-language.ps1
+++ vagrant/win32/test-language.ps1
@@ -15,7 +15,7 @@ $HereDir = (get-item $HereScript).Directory
 $opts = "--ci"
 
 if (Test-Path $HereDir\setup.exe) {
-  Write-Host "Running supplied setup.exe ..."
+  Write-Output "Running supplied setup.exe ..."
   & "$HereDir\setup.exe" $opts
 
 } else {
@@ -31,14 +31,14 @@ if (Test-Path $HereDir\setup.exe) {
   $TagVersion = (Get-Content $HereDir\tagversion.txt -TotalCount 1).Trim()
 
   $url = "https://github.com/diskuv/dkml-installer-ocaml/releases/download/$TagVersion/setup-diskuv-ocaml-windows_x86_64-$OpamVersion.exe"
-  Write-Host "Downloading from $url ..."
+  Write-Output "Downloading from $url ..."
   Invoke-WebRequest $url -OutFile "$env:TEMP\setup.exe"
 
-  Write-Host "Running setup.exe ..."
+  Write-Output "Running setup.exe ..."
   & "$env:TEMP\setup.exe" $opts
 }
 
-Write-Host "Done installation."
+Write-Output "Done installation."
 
 # END Install instructions
 # ========================
@@ -51,11 +51,11 @@ $env:Path = [Environment]::GetEnvironmentVariable('PATH', 'User') + [System.IO.P
 # Mimic $DiskuvOCamlHome
 $env:DiskuvOCamlHome = "$env:LOCALAPPDATA\Programs\DiskuvOCaml"
 
-Write-Host "Printing installation ..."
+Write-Output "Printing installation ..."
 opam var
 $opamroot = opam var root
 
-Write-Host "Testing installation ..."
+Write-Output "Testing installation ..."
 # Clean, run test for installation and save results
 dune clean --root C:\vagrant\test_installation.t
 if ($lastexitcode -ne 0) { throw ("FATAL: dune clean failed") }
@@ -63,7 +63,7 @@ dune runtest --root C:\vagrant\test_installation.t --display=short
 if ($lastexitcode -ne 0) { throw ("FATAL: dune runtest failed") }
 Set-Content -Path "C:\vagrant\test_installation.t\exitcode.$SystemLocale.txt" -Value $LastExitCode -NoNewline -Encoding Ascii
 
-Write-Host "Testing otherplayground ..."
+Write-Output "Testing otherplayground ..."
 # Run through a simple playground ... different from the auto-installed 'playground' switch ...
 # where we can test a new switch being created
 (Test-Path -Path C:\vagrant\otherplayground) -or $(New-Item C:\vagrant\otherplayground -ItemType Directory)
@@ -73,14 +73,14 @@ opam dkml init --build-type=Release --yes # `Release` option is present simply t
 if ($lastexitcode -ne 0) { throw ("FATAL: opam dkml init failed") }
 opam install graphics --yes       # install something with a low number of dependencies, that sufficienly exercises Opam
 if ($lastexitcode -ne 0) {
-  Write-Host "----- TROUBLESHOOTING: dune-*-*.out ----"
-  Get-ChildItem "$opamroot\log" -Filter "dune-*-*.out" | ForEach-Object { Write-Host "[BEGIN $_]" ; Get-Content $_; Write-Host "[END $_]`n" }
+  Write-Output "----- TROUBLESHOOTING: dune-*-*.out ----"
+  Get-ChildItem "$opamroot\log" -Filter "dune-*-*.out" | ForEach-Object { Write-Output "[BEGIN $_]" ; Get-Content $_; Write-Output "[END $_]`n" }
   throw ("FATAL: opam install graphics failed")
 }
 opam install ppx_jane --yes       # regression test: https://discuss.ocaml.org/t/ann-diskuv-ocaml-1-x-x-windows-ocaml-installer-no-longer-in-preview/10309/8?u=jbeckford
 if ($lastexitcode -ne 0) { throw ("FATAL: opam install ppx_jane failed") }
 
-Write-Host "Done tests."
+Write-Output "Done tests."
 
 # END tests
 # ========================
@@ -93,9 +93,9 @@ $EstimatedSizeB = (Get-ChildItem $env:DiskuvOCamlHome -Recurse | Measure-Object 
 #   ex. 1429494.69726563 (1.4 GB)
 $EstimatedSizeKb = $EstimatedSizeB / 1KB
 #   ex. estimated_byte_size_opt=Some 1463802570
-Write-Host ("estimated_byte_size_opt=Some {0}L" -f $EstimatedSizeB)
+Write-Output ("estimated_byte_size_opt=Some {0}L" -f $EstimatedSizeB)
 #   ex. EstimatedSize=1,429,494.70 KB
-Write-Host ("EstimatedSize={0:N2} KB" -f $EstimatedSizeKb)
+Write-Output ("EstimatedSize={0:N2} KB" -f $EstimatedSizeKb)
 #   ex. 0015cff6
 $EstimatedSizeDwordKb = ("{0:x}" -f [int]$EstimatedSizeKb).PadLeft(8, "0")
 Set-Content -Path "C:\vagrant\test_installation.t\estimatedsize.$SystemLocale.txt" `
@@ -109,7 +109,7 @@ Set-Content -Path "C:\vagrant\test_installation.t\estimatedsize.$SystemLocale.tx
 # START uninstaller
 
 if (Test-Path $HereDir\uninstall.exe) {
-  Write-Host "Running supplied uninstall.exe ..."
+  Write-Output "Running supplied uninstall.exe ..."
   & "$HereDir\uninstall.exe" $opts
 
 } else {
@@ -125,14 +125,14 @@ if (Test-Path $HereDir\uninstall.exe) {
   $TagVersion = (Get-Content $HereDir\tagversion.txt -TotalCount 1).Trim()
 
   $url = "https://github.com/diskuv/dkml-installer-ocaml/releases/download/$TagVersion/uninstall-diskuv-ocaml-windows_x86_64-$OpamVersion.exe"
-  Write-Host "Downloading from $url ..."
+  Write-Output "Downloading from $url ..."
   Invoke-WebRequest $url -OutFile "$env:TEMP\uninstall.exe"
 
-  Write-Host "Running uninstall.exe ..."
+  Write-Output "Running uninstall.exe ..."
   & "$env:TEMP\uninstall.exe" $opts
 }
 
-Write-Host "Done uninstallation."
+Write-Output "Done uninstallation."
 
 # END uninstaller
 # ========================
