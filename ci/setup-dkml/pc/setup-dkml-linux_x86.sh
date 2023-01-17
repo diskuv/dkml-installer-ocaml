4f6c9535910d27526f82cc38f711667b5337bb27
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2023-01-17 04:12:55 -0800

    1.2.0~prerel8

--- ci/setup-dkml/pc/setup-dkml-linux_x86.sh
+++ ci/setup-dkml/pc/setup-dkml-linux_x86.sh
@@ -37,15 +37,15 @@ export PIN_CORE_KERNEL='v0.15.0'
 export PIN_CTYPES_FOREIGN='0.19.2-windowssupport-r5'
 export PIN_CTYPES='0.19.2-windowssupport-r5'
 export PIN_CURLY='0.2.1-windows-env_r2'
-export PIN_DKML_APPS='1.2.0~prerel7'
-export PIN_DKML_EXE='1.2.0~prerel7'
+export PIN_DKML_APPS='1.2.0~prerel8'
+export PIN_DKML_EXE='1.2.0~prerel8'
 export PIN_DUNE='3.6.2'
 export PIN_FEATHER='0.3.0'
 export PIN_OCAMLBUILD='0.14.0'
 export PIN_OCAMLFIND='1.9.1'
 export PIN_OCP_INDENT='1.8.2-windowssupport'
 export PIN_PPX_EXPECT='v0.15.1'
-export PIN_WITH_DKML='1.2.0~prerel7'
+export PIN_WITH_DKML='1.2.0~prerel8'
 
 usage() {
   echo 'Setup Diskuv OCaml (DKML) compiler on a desktop PC.' >&2
@@ -439,9 +439,9 @@ set -euf
 # Constants
 SHA512_DEVNULL='cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e'
 #   Edited by https://gitlab.com/diskuv/diskuv-ocaml/contributors/release.sh
-DEFAULT_DISKUV_OPAM_REPOSITORY_TAG=f3524ea53e3755a941c5ed75054a6bbdeb360191
+DEFAULT_DISKUV_OPAM_REPOSITORY_TAG=638195f6db6f54f121a7211515f26d946494eb95
 # Constants
-DKML_VERSION=1.2.0-prerel7
+DKML_VERSION=1.2.0-prerel8
 
 setup_WORKSPACE_VARNAME=$1
 shift
