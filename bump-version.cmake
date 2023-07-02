736551f79ca51714a5361a27752b5681a7ba9740
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2023-07-01 21:28:11 -0700

    Local, semver compliant version bumping

---
+++ bump-version.cmake
@@ -0,0 +1,16 @@
+# Test with:
+# cmake --log-context -D DRYRUN=1 -D CMAKE_MESSAGE_CONTEXT=dkml-installer-ocaml -D "regex_DKML_VERSION_SEMVER=1[.]2[.]1-[0-9]+" -D "regex_DKML_VERSION_OPAMVER=1[.]2[.]1[~]prerel[0-9]+" -D DKML_VERSION_SEMVER_NEW=1.2.1-3 -D DKML_VERSION_OPAMVER_NEW=1.2.1~prerel3 -D GIT_EXECUTABLE=git -D DKML_RELEASE_OCAML_VERSION=4.14.0 -D DKML_BUMP_VERSION_PARTICIPANT_MODULE=../../../pkg/bump/DkMLBumpVersionParticipant.cmake -P bump-version.cmake
+
+if(NOT DKML_BUMP_VERSION_PARTICIPANT_MODULE)
+    message(FATAL_ERROR "Missing -D DKML_BUMP_VERSION_PARTICIPANT_MODULE=.../DkMLBumpVersionParticipant.cmake")
+endif()
+include(${DKML_BUMP_VERSION_PARTICIPANT_MODULE})
+
+DkMLBumpVersionParticipant_PlainReplace(README.md)
+DkMLBumpVersionParticipant_PlainReplace(installer/winget/manifest/Diskuv.OCaml.installer.yaml)
+DkMLBumpVersionParticipant_PlainReplace(installer/winget/manifest/Diskuv.OCaml.locale.en-US.yaml)
+DkMLBumpVersionParticipant_PlainReplace(installer/winget/manifest/Diskuv.OCaml.yaml)
+DkMLBumpVersionParticipant_ReleaseDateReplace(installer/winget/manifest/Diskuv.OCaml.installer.yaml)
+DkMLBumpVersionParticipant_CopyrightReplace(installer/winget/manifest/Diskuv.OCaml.locale.en-US.yaml)
+DkMLBumpVersionParticipant_DuneProjectReplace(dune-project)
+DkMLBumpVersionParticipant_GitAddAndCommit()
