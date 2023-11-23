c58aa0ccd87c248e775ead12d4e29eae89c1c019
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2023-11-23 00:26:26 -0800

    Document program_name
    Part of https://github.com/diskuv/dkml-installer-ocaml/issues/80

--- i-network/src/private_common.ml
+++ i-network/src/private_common.ml
@@ -6,8 +6,11 @@ let build_info = { Author_types.package_name = "dkml-installer-ocaml-network" }
 let program_name =
   {
     Author_types.name_full = "DkML Native";
+
+    (* Must be in sync with dkml-runtime-apps/src/runtimelib/dkml_context.ml for [dkml init --system], etc. *)
     name_camel_case_nospaces = "DkMLNative";
     name_kebab_lower_case = "dkml-native";
+
     installation_prefix_camel_case_nospaces_opt = None;
     installation_prefix_kebab_lower_case_opt = None;
   }
